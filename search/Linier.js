822070159fc793a2ae1412fbeffa753d075bb712
arun <arunks002info@gmail.com>
2024-05-30 13:11:34 +0530

    binary-search

---
+++ search/Linier.js
@@ -0,0 +1,22 @@
+
+function linearSearch(array,key)
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let ar=[1,33,4,53,23,34]
+console.log(linearSearch(ar,23))
---
+++ search/binary.js
@@ -0,0 +1,36 @@
+function binarySearch(array,key,start,end)
+{
+    
+     while(start<=end)
+        {
+        let mid = Math.floor((start + end) / 2);
+        if(key===array[mid])
+            return true;
+         else if(key<array[mid])
+            end =mid-1;
+        else (key>array[mid])
+        start=mid+1;
+        }
+      return false
+    
+}
+
+
+let arr = [1, 3, 5, 7, 8, 9];
+let x = 5;
+
+if (binarySearch(arr, x, 0, arr.length - 1)) {
+	console.log("Element found!");
+}
+else {
+	console.log("Element not found!");
+}
+
+x = 8;
+
+if (binarySearch(arr, x, 0, arr.length - 1)) {
+	console.log("Element found!");
+}
+else {
+	console.log("Element not found!");
+}
